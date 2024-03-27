3374a2c7bcbc8e47d26ce6027e4020b82e246755
minnhhttris <ntmtri02@gmail.com>
2024-03-27 14:44:52 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+
+}
+</script>
+
+<template>
+  <h1>Hello, Vue.js</h1>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
